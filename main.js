9e3466351d72044ef95e13b106b7ac90fa2584a9
Priya Choudhary <priyac1411@gmail.com>
2020-06-14 17:48:06 +0530

    implemented dark mode

--- main.js
+++ main.js
@@ -62,3 +62,56 @@ $(document).ready(function() {
   });
 
 });
+
+
+// Dark Mode Implemented using DOM Manipulation of css variables
+
+const colorWhite = '#ffffff';
+const bgColorDark = '#282c35';
+const bgColorLight = '#edecec';
+const itColor = '#959595';
+const primeColor = '#5a5a5a';
+const secondColor = '#7c746c';
+const shadowDarkOnLight = '#cccccc';
+const shadowLightOnLight = '#fefefe';
+const shadowDarkOnDark = '#22252d';
+const shadowLightOnDark = '#2e333d';
+
+let root = document.documentElement;
+
+const mode = document.querySelector('.mode');
+const darkMode = document.querySelector('#dark-mode');
+const lightMode = document.querySelector('#light-mode');
+
+function changeModeToDark () {
+  darkMode.classList.add('hide');
+  lightMode.classList.remove('hide');
+
+  darkMode.classList.remove('show');
+  lightMode.classList.add('show');
+  root.style.setProperty('--background-color', bgColorDark);
+  root.style.setProperty('--it-color', colorWhite);
+  root.style.setProperty('--prime-color', colorWhite);
+  root.style.setProperty('--second-color', colorWhite);
+  root.style.setProperty('--shadow-dark', shadowDarkOnDark);
+  root.style.setProperty('--shadow-light', shadowLightOnDark);
+
+}
+
+function changeModeToLight () {
+  lightMode.classList.add('hide');
+  darkMode.classList.add('show');
+
+  lightMode.classList.remove('show');
+  darkMode.classList.remove('hide');
+  root.style.setProperty('--background-color', bgColorLight);
+  root.style.setProperty('--it-color', itColor);
+  root.style.setProperty('--prime-color', primeColor);
+  root.style.setProperty('--second-color', secondColor);
+  root.style.setProperty('--shadow-dark', shadowDarkOnLight);
+  root.style.setProperty('--shadow-light', shadowLightOnLight);
+
+}
+
+darkMode.addEventListener('click', changeModeToDark);
+lightMode.addEventListener('click', changeModeToLight);
